27365b285e06b56c98a7c7e3ff052c489f75f559
Ashlesa12 <ashlesaamatya123@gmail.com>
2026-07-21 21:56:57 +0545

    initial pennyFlow backend setup with FastAPI and PostgreDQL

---
+++ backend/app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+from .database import Base, engine
+from .routes.auth import router as auth_router
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(
+    title="PennyFlow API"
+)
+
+app.include_router(auth_router)
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "Welcome to PennyFlow 🚀"
+    }
