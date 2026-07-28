950e6bb3562268fe434af7d782ffc5158f686614
Ashlesa12 <ashlesaamatya123@gmail.com>
2026-07-28 20:01:53 +0545

    frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900">
+      <h1 className="text-5xl font-bold text-emerald-500">
+        PennyFlow 💰
+      </h1>
+    </div>
+  );
+}
+
+export default App;
